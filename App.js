e063059feb73d46e0701120a2556e53c8f356ba9
EthanPui <ethanpui@gmail.com>
2020-12-29 12:05:47 +0800

    Added Image with blur and fadeduration

--- App.js
+++ App.js
@@ -1,14 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView} from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView} from 'react-native';
+
+
 
 export default function App() {
-
-const handlePress = () => console.log("Text pressed");
-
   return (
     <SafeAreaView style={styles.container}>
-      <Text numberOfLines={1}>Hello React Native - A really really long text. Now its even longer too see what happens!</Text>
+      <Text>Hello React Native </Text>
+      <Image 
+      blurRadius={1}
+      fadeDuration={1000}
+        source={{
+          width: 200,
+          height: 300,
+          uri: "https://picsum.photos/200/300",
+        }} 
+      />
     </SafeAreaView>     
   );
 }
@@ -16,6 +24,8 @@ const handlePress = () => console.log("Text pressed");
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'dodgerblue',
+    backgroundColor: '#fff',
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
